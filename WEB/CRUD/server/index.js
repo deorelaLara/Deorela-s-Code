547e4ab931b150fe487119c86fadb300b3400775
deorelaLara <l.lara@uadec.edu.mx>
2020-09-23 23:26:43 -0500

    conexion con bd funcionando

--- WEB/CRUD/server/index.js
+++ WEB/CRUD/server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db=mysql.createPool({
+    host : 'localhost',
+    user: 'root',
+    password : '',
+    database : 'cruddataBase'
+})
 
 app.get("/", (req, res)=>{
-    res.send("Hello world")
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Inception', 'good movie');"
+    db.query(sqlInsert, (req, result)=>{
+    res.send("Hello world!! IT WORKS");
+})
 })
 
 app.listen(3001, () => {
